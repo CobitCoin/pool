f1ae1ef167fd4c51eadc4d5599bd70de6e874b46
Cobitcoin <cobit@cobitcoin.io>
2018-06-25 23:38:55 -0300

    Cobitcoin Minig Pool
    first update

---
+++ website/config.js
@@ -0,0 +1,25 @@
+var api = "https://api.cobitcoin.io";
+
+var api_blockexplorer = "https://apiblock.cobitcoin.io";
+
+var poolHost = "pool.cobitcoin.io";
+
+var irc = "";
+
+var email = "support@cobitcoin.io";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://blockchain.cobitcoin.io";
+
+var transactionExplorer = "https://blockchain.cobitcoin.io";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "CBC": [
+        ["pool.cobitcoin.io", "pool.cobitcoin.io:3333"]
+    ]
+};
